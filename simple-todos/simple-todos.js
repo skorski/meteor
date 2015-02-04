c923141a8be00a3af50667f11d82c3b83e9bcbcc
Dan Skorski <skorsk@gmail.com>
2015-02-03 21:03:33 -0800

    Removed insecure and added users.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -5,7 +5,17 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      if (Session.get("hideCompleted")) {
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -13,19 +23,56 @@ if (Meteor.isClient) {
     "submit .new-task": function(event) {
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call("addTask", text);
 
       event.target.text.value = "";
 
       return false;
-    }
+    },
 
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function(){
+      Meteor.call("setChecked", this._id, ! this.checked);
+    },
+
+    "click .delete ": function() {
+      Meteor.call("deleteTask", this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+     Tasks.insert({
+        text: text,
+        owner: Meteor.userId(),
+        username: Meteor.user().username,
+        createdAt: new Date()
+      });
+  },
+
+  deleteTask: function (taskID) {
+    Tasks.remove(taskID);
+  },
+
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
